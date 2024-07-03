43f88e88ca5ee72a10fca23e19e895629cc96384
Jorao Jr <jorgomjr@gmail.com>
2024-07-03 16:03:07 +0100

    Finishing Exit Survey

--- post_survey/__init__.py
+++ post_survey/__init__.py
@@ -11,11 +11,26 @@ class C(BaseConstants):
     NUM_ROUNDS = 1
 
     LIKERT = [
-        [1, 'Strongly disagree'],
-        [2, 'Disagree'],
-        [3, 'Neither Agree nor Disagree'],
-        [4, 'Agree'],
-        [5, 'Strongly agree']
+        [0, ''],
+        [1, ''],
+        [2, ''],
+        [3, ''],
+        [4, ''],
+        [5, ''],
+        [6, ''],
+        [7, ''],
+        [8, ''],
+        [9, ''],
+        [10, '']
+    ]
+
+    POSITIVE_RECIPROCITY = [
+        [5, ''],
+        [10, ''],
+        [15, ''],
+        [20, ''],
+        [25, ''],
+        [30, ''],
     ]
 
 
@@ -30,7 +45,7 @@ class Group(BaseGroup):
 class Player(BasePlayer):
     age = models.IntegerField(
         label="How old are you?",
-        min=18,
+        min=14,
         max=100
     )
 
@@ -49,8 +64,50 @@ class Player(BasePlayer):
         choices=["Strongly Disagree", "Disagree", "Neither Agree nor Disagree", "Agree", "Strongly Agree"]
     )
 
-    QA1 = models.IntegerField(label='Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing',
-                              choices=C.LIKERT, widget=widgets.RadioSelectHorizontal())
+    """
+        LEGEND:
+        RT = Risk Taking
+        T = TRUST
+        A - Altruism
+        NR = Negative reciprocity 
+        PR = Positive reciprocity
+        
+    """
+
+    RT_QA1 = models.IntegerField(
+        label='How do you see yourself: Are you a person who is generally willing to take risks, or do you try to avoid taking risks?',
+        choices=C.LIKERT,
+        widget=widgets.RadioSelectHorizontal()
+    )
+
+    T_QA1 = models.IntegerField(
+        label='How well does the following statement describe you as a person? As long as I am not convinced otherwise, I assume that people have '
+              'only the best intentions.   ',
+        choices=C.LIKERT,
+        widget=widgets.RadioSelectHorizontal()
+    )
+
+    A_QA1 = models.IntegerField(
+        label='How do you assess your willingness to share with others without expecting anything in return when it comes to charity?',
+        choices=C.LIKERT,
+        widget=widgets.RadioSelectHorizontal()
+    )
+
+    NR_QA1 = models.IntegerField(
+        label='How do you see yourself: Are you a person who is generally willing to punish unfair behavior even if this is costly?',
+        choices=C.LIKERT,
+        widget=widgets.RadioSelectHorizontal()
+    )
+
+    PR_QA1 = models.IntegerField(
+        label='Imagine the following situation: you are shopping in an unfamiliar city and realize you lost your way. You ask a stranger for '
+              'directions. The stranger offers to take you with their car to your destination. The ride takes about 20 minutes and costs the '
+              'stranger about 20 Euro in total. The stranger does not want money for it. You carry six bottles of wine with you. The cheapest '
+              'bottle costs 5 Euro, the most expensive one 30 Euro. You decide to give one of the bottles to the stranger as a thank-you gift. '
+              'Which bottle do you give?',
+        choices=C.POSITIVE_RECIPROCITY,
+        widget=widgets.RadioSelectHorizontal()
+    )
 
 
 class Demographics(Page):
@@ -70,7 +127,12 @@ class Introduction(Page):
 
 class Q1(Page):
     form_model = 'player'
-    form_fields = ['QA1']
+    form_fields = ['RT_QA1', 'T_QA1', 'A_QA1', 'NR_QA1']
+
+
+class Q2(Page):
+    form_model = 'player'
+    form_fields = ['PR_QA1']
 
 
 class FinalPage(Page):
@@ -84,4 +146,4 @@ class FinalPage(Page):
         )
 
 
-page_sequence = [Introduction, Q1, Demographics, FinalPage]
+page_sequence = [Introduction, Q1, Q2, Demographics, FinalPage]
--- settings.py
+++ settings.py
@@ -27,38 +27,6 @@ ROOMS = [
     },
 ]
 
-# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
-# in SESSION_CONFIGS, except those that explicitly override it.
-# the session config can be accessed from methods in your apps as self.session.config,
-# e.g. self.session.config['participation_fee']
-
-SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=0.0005,  # 0.05% => 2000 ECU = 1 EUR, 0.10% => 1000 ECU = 1 EUR
-    participation_fee=5.00,
-    draw_earnings_num_rounds=5,
-    doc="",
-)
-
-PARTICIPANT_FIELDS = ["treatment", "transfer_price", "inventory_order_history", "demand_history", "earnings_list", "draw_earnings_indexes",
-                      "drawn_earnings", "avg_earnings"]
-SESSION_FIELDS = []
-
-# ISO-639 code
-# for example: de, fr, ja, ko, zh-hans
-LANGUAGE_CODE = 'en'
-
-# e.g. EUR, GBP, CNY, JPY
-REAL_WORLD_CURRENCY_CODE = 'EUR'
-USE_POINTS = True
-
-ADMIN_USERNAME = 'admin'
-# for security, best to set admin password in an environment variable
-ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
-
-DEMO_PAGE_INTRO_HTML = """ """
-
-SECRET_KEY = '6733260475510'
-
 GAME_CONFIG_DEFAULTS = dict(
     num_rounds=5,
     treatments={
@@ -151,3 +119,35 @@ GAME_CONFIG_DEFAULTS = dict(
 
     }
 )
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=0.0005,  # 0.05% => 2000 ECU = 1 EUR, 0.10% => 1000 ECU = 1 EUR
+    participation_fee=5.00,
+    draw_earnings_num_rounds=5,
+    doc="",
+)
+
+PARTICIPANT_FIELDS = ["treatment", "transfer_price", "inventory_order_history", "demand_history", "earnings_list", "draw_earnings_indexes",
+                      "drawn_earnings", "avg_earnings"]
+SESSION_FIELDS = []
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'EUR'
+USE_POINTS = True
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+
+SECRET_KEY = '6733260475510'
